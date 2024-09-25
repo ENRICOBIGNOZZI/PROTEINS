0727b4815eb39e9b7d5ef2905a1025465014ccfb
Enrico Bignozzi <bignoenir1998e@gmail.com>
2024-09-25 10:05:20 +0200

    adoi

--- STATIC/2_temp_sferic.py
+++ STATIC/2_temp_sferic.py
@@ -4,35 +4,37 @@ from Downlaod_data import PDBProcessor
 from Visualize import Visualize
 from matrix import GraphMatrixAnalyzer
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import os
-stringa="2m10"
-pdb_processor = PDBProcessor(pdb_id="2m10")
+import os
+import matplotlib.lines as mlines
+import matplotlib.patches as mpatches
+stringa="3LNX"
+pdb_processor = PDBProcessor(pdb_id="3LNX")#2m07
 pdb_processor.download_pdb()
 pdb_processor.load_structure()
-
-# Extract data
 df1 = pdb_processor.secondary_structure()
 df = pdb_processor.extract_atom_data()
-print(df)
 df = df[df['Model ID'] == 0]
 df = df[df['Atom Name'] == 'CA']
-
+df = df[df['Chain ID'] == 'A']
 df = df.reset_index(drop=True)
 concatenated_df = pd.concat([df1['Secondary Structure'], df], axis=1)
 df = concatenated_df.dropna().reset_index(drop=True)
-# Initialize Visualize and analyze graph
+df = df.loc[:,~df.T.duplicated()]
+df = concatenated_df.dropna().reset_index(drop=True)
+df = df.T.drop_duplicates().T
+df=df.dropna().reset_index(drop=True)
 visualizer = Visualize(df)
+
 raggio=visualizer.calculate_and_print_average_distance()
 #visualizer.plot_connections_vs_radius()
 G = visualizer.create_and_print_graph(truncated=True, radius=8.0, plot=False, peso=1)  # Adjust radius as needed
 analyzer = GraphMatrixAnalyzer(G)
 kirchhoff_matrix = analyzer.get_kirchhoff_matrix()
-
-
-
+df[['X', 'Y', 'Z']] = df[['X', 'Y', 'Z']].apply(pd.to_numeric, errors='coerce')
 centro_massa = df[['X', 'Y', 'Z']].mean().values
-
-# Calcolo delle distanze dal centro di massa
 distanze = np.sqrt(((df[['X', 'Y', 'Z']] - centro_massa) ** 2).sum(axis=1))
 
 # Parametri per la temperatura radiale
@@ -43,24 +45,6 @@ R = distanze.max()  # Raggio massimo
 # Calcolo della temperatura radiale
 temperatura_radiale = T0 + (Tb - T0) / R * distanze
 
-print("Centro di massa:", centro_massa)
-print("Raggio massimo:", R)
-print("Primi 5 valori di distanza:", distanze[:5])
-print("Primi 5 valori di temperatura radiale:", temperatura_radiale[:5])
-def calculate_Cij(u, Q,lambdaa, i,j,s,t):
-    Cij = np.zeros((u.shape[0], u.shape[0]))
-    if s>t:
-        for k in range(1,u.shape[0]):
-            for p in range(1,u.shape[0]):
-                Cij[i][j] += ((u[i][k] * Q[k][p] * u[j][p]) / (lambdaa[k] + lambdaa[p]))*np.exp(-lambdaa[p]*(s-t))
-    if s<t:
-        for k in range(1,u.shape[0]):
-            for p in range(1,u.shape[0]):
-                Cij[i][j] += ((u[i][k] * Q[k][p] * u[j][p]) / (lambdaa[k] + lambdaa[p]))*np.exp(-lambdaa[k]*(t-s))
-    return Cij
-
-import numpy as np
-
 def calculate_Q(U, B):
     B_transpose = np.transpose(B)
     U_transpose = np.transpose(U)
@@ -70,35 +54,10 @@ def calculate_Q(U, B):
 eigenvalues, eigenvectors = np.linalg.eig(kirchhoff_matrix)
 
 Q=calculate_Q(  eigenvectors,temperatura_radiale)
-print(Q)
-print(Q.shape)
-Cij=calculate_Cij(eigenvectors, Q, eigenvalues, i=20,j=70,s=0,t=1)
-
-def calculate_Cij_matrix_static(u, Q, lambdaa,t,s):#questa è quella corretta
-    print(np.dot(u,np.dot(Q,u)))
-    print(np.dot(u,np.dot(Q,u)).shape)
-    print(np.sum(lambdaa + lambdaa))
-    print(np.sum(lambdaa + lambdaa).shape)
-    if t>s
-        Cij= np.dot(u,np.dot(Q,u))/ np.sum(lambdaa + lambdaa)*np.exp(-lambdaa*(t-s))
-    else:
-        Cij= np.dot(u,np.dot(Q,u))/ np.sum(lambdaa + lambdaa)*np.exp(-lambdaa*(s-t))
-    return Cij
-# Calcola la matrice Cij per tutti gli ij
-#Cij_matrix = calculate_Cij_matrix(eigenvectors, Q, eigenvalues, s=0, t=1)
-
-Cij_matrix = calculate_Cij_matrix_static(eigenvectors, Q, eigenvalues)
-print(Cij_matrix)
-print(Cij_matrix.shape)
-# Crea un plot della matrice di correlazione
-plt.figure(figsize=(10, 10))
-plt.imshow(Cij_matrix, cmap='hot', interpolation='nearest')
-plt.colorbar(label='Correlation')
-plt.title('Correlation Matrix')
-plt.show()
 
 
-'''residui = range(1, len(temperatura_radiale) + 1)
+
+residui = range(1, len(temperatura_radiale) + 1)
 
 plt.figure(figsize=(12, 6))
 plt.scatter(residui, temperatura_radiale, c=temperatura_radiale, cmap='coolwarm', marker='o')
@@ -116,46 +75,169 @@ if not os.path.exists(f'images/{stringa}/2_temperature_sferical/'):
 plt.savefig(f'images/{stringa}/2_temperature_sferical/temperature_sferic.png')
 
 # Grafico 3D della proteina colorata per temperatura
+def plot_matrix( matrix, secondary_structure, nome, title="Matrix"):
+    binary_matrix=matrix
+    fig, ax = plt.subplots(figsize=(10, 10))
+    
+    # Create a binary matrix: 1 where connections exist, 0 otherwise
+    
+    
+    # Get the indices of non-zero elements
+    
+    
+    # Plot the dots
+    cax = ax.imshow(matrix, cmap='viridis')
 
+    # Aggiunta della barra del colore (opzionale)
+    fig.colorbar(cax)
+
+    # Add rectangles
+    rectangle1 = patches.Rectangle((19, 71), 5, 9, linewidth=2, edgecolor='r', facecolor='none')
+    rectangle2 = patches.Rectangle((71, 19), 9, 5, linewidth=2, edgecolor='r', facecolor='none')
+    ax.add_patch(rectangle1)
+    ax.add_patch(rectangle2)
+    
+    # Create segments on x and y axes based on secondary structure
+    start = 0
+    current_structure = secondary_structure[0]
+    if len(secondary_structure) >= 2:
+        current_structure = current_structure[0]
+    
+    for i, structure in enumerate(secondary_structure):
+        if len(structure) >= 2:
+            structure = structure[0]
+    
+        if structure != current_structure:
+            if current_structure == 'H' or current_structure == 'E':  # Plot only for Helix and Beta Sheet
+                color = 'red' if current_structure == 'H' else 'blue'
+                ax.plot([start, i], [0, 0], color=color, linewidth=8)  # Increase line thickness
+                ax.plot([0, 0], [start, i], color=color, linewidth=8)  # Increase line thickness
+                ax.text((start+i)/2, -0.5, current_structure, ha='center', va='top', fontsize=12, fontweight='bold')
+                ax.text(-0.5, (start+i)/2, current_structure, ha='right', va='center', fontsize=12, fontweight='bold')
+            start = i
+            current_structure = structure
+    if current_structure == 'H' or current_structure == 'E':  # Plot final segment if it is Helix or Beta Sheet
+        color = 'red' if current_structure == 'H' else 'blue'
+        ax.plot([start, i+1], [0, 0], color=color, linewidth=8)  # Increase line thickness
+        ax.plot([0, 0], [start, i+1], color=color, linewidth=8)  # Increase line thickness
+        ax.text((start+i+1)/2, -0.5, current_structure, ha='center', va='top', fontsize=12, fontweight='bold')
+        ax.text(-0.5, (start+i+1)/2, current_structure, ha='right', va='center', fontsize=12, fontweight='bold')
+    
+    # Add legend for Helix and Beta Sheet only
+    handles = [
+        patches.Patch(color='red', label='Helix'),
+        patches.Patch(color='blue', label='Beta Sheet')
+    ]
+    ax.legend(handles=handles, loc='upper right')
+    
+    #ax.set_title(title)
+    ax.set_xlabel('Residue Index')
+    ax.set_ylabel('Residue Index')
+    ax.set_ylim(0, binary_matrix.shape[0])
+    ax.set_xlim(0, binary_matrix.shape[1])
+    
+    plt.tight_layout()
+    if not os.path.exists(f'images/{stringa}/2_temperature_sferical/'):
+        os.makedirs(f'images/{stringa}/2_temperature_sferical/')
+    plt.savefig(f'images/{stringa}/2_temperature_sferical/correlation.png')
 
 import scipy.integrate as integrate
 
 # Assicuriamoci che temperatura_radiale sia un array numpy
 temperatura_radiale = np.array(temperatura_radiale)
 
-# Funzione da integrare
-def integrando(u, K, T):
-    exp_Ku = np.exp(K * u)
-    return exp_Ku * T[:, np.newaxis] * T[np.newaxis, :] * exp_Ku
 
 # Funzione per calcolare l'integrale
-def calcola_integrale(K, T, limite_inferiore=-100, num_punti=1000):
-    def func(u):
-        return integrando(u, K, T)
-    
-    risultato, _ = integrate.quad_vec(func, limite_inferiore, 0, limit=num_punti)
-    return risultato
+
 
 # Calcolo dell'integrale
 K = 1.0  # Puoi modificare questo valore secondo le tue necessità
-risultato_integrale = calcola_integrale(K, temperatura_radiale)
 
-print("Dimensioni del risultato dell'integrale:", risultato_integrale.shape)
-print("Primi 5x5 elementi del risultato:")
-print(risultato_integrale[:5, :5])
+def calculate_Cij_matrix_static(u, Q, lambdaa,t,s):#questa è quella corretta
+    if t>s:
+        Cij= np.dot(u,np.dot(Q,u))/ np.sum(lambdaa + lambdaa)*np.exp(-lambdaa*(t-s))
+    else:
+        Cij= np.dot(u,np.dot(Q,u))/ np.sum(lambdaa + lambdaa)*np.exp(-lambdaa*(s-t))
+    return Cij
+# Calcola la matrice Cij per tutti gli ij
+#Cij_matrix = calculate_Cij_matrix(eigenvectors, Q, eigenvalues, s=0, t=1)
 
-# Visualizzazione del risultato come heatmap
-plt.figure(figsize=(10, 8))
-plt.imshow(risultato_integrale, cmap='viridis', aspect='auto')
-plt.colorbar(label='Valore dell\'integrale')
-plt.title(f'Heatmap del risultato dell\'integrale (K={K})')
-plt.xlabel('Indice del residuo')
-plt.ylabel('Indice del residuo')
-plt.tight_layout()
+t=0.1
+s=0
+Cij_matrix = calculate_Cij_matrix_static(eigenvectors, Q, eigenvalues,t,s)
+plot_matrix( Cij_matrix ,  df['Secondary Structure'].values, stringa, title="Matrix_sferic_temperature")
+'''plt.figure(figsize=(12, 6))
+plt.imshow(Cij_matrix, cmap='hot', interpolation='nearest')
+plt.colorbar(label='Correlation Matrix t={}, s={}'.format(t,s))  
+plt.title('Correlation Matrix')
 if not os.path.exists(f'images/{stringa}/2_temperature_sferical/'):
     os.makedirs(f'images/{stringa}/2_temperature_sferical/')
-
-# Save the figure
 plt.savefig(f'images/{stringa}/2_temperature_sferical/correlation.png')'''
 
 
+def plot_residual_correlation_vs_j(self, i, t, time_idx):
+    residual_correlation_matrix = self.compute_residual_correlation_matrix(t, i, time_idx)
+    print("inzio correlation")
+    print(residual_correlation_matrix.shape)
+    print("fine correlation")
+    self._plot_with_secondary_structure(residual_correlation_matrix, f'Correlation with i={i}', f'Residual Correlation C_ij for i={i} as a function of j at time index {time_idx}')
+def compute_residual_correlation_matrix(self, t,i, time_idx):
+    """Calcola la matrice di correlazione dei residui per tutti i j e per un intervallo di tempo specificato."""
+    n = self.u.shape[0]
+    t_subset=t
+    # Inizializza una matrice per le correlazioni temporali
+    residual_correlation_matrix = np.zeros((n, n, len(t_subset)))
+
+    for j in range(n):
+        residual_correlation_matrix[i, j, :] = self.time_correlation_2(i, j, t_subset)
+    return residual_correlation_matrix[i,:,:]
+
+def _plot_with_secondary_structure(self, matrix, ylabel, title):
+        sec_struct_info = self.sec_struct_data['Secondary Structure']
+        residue_ids = self.sec_struct_data['Residue ID'].astype(int)
+
+        # Colors only for 'H' (alpha-helix) and 'E' (beta-sheet)
+        colors = {'H': 'red', 'E': 'blue'}
+        sec_struct_colors = ['white'] * len(residue_ids)  # Default to white for all residues
+
+        # Assign colors to residues based on their secondary structure
+        for idx, rid in enumerate(residue_ids):
+            struct = sec_struct_info.get(rid, 'C')  # Default to 'C' if not found
+            if struct in colors:
+                sec_struct_colors[idx] = colors[struct]
+
+        plt.figure(figsize=(12, 8))
+        plt.plot(range(len(matrix)), matrix, marker='o', linestyle='-', alpha=0.7)
+
+        # Plot the secondary structure bands
+        current_color = 'white'
+        start_idx = 0
+        for idx, resid in enumerate(residue_ids):
+            if sec_struct_colors[idx] != current_color:
+                if idx > 0 and current_color in colors.values():
+                    plt.axvspan(start_idx, idx, color=current_color, alpha=0.2)
+                current_color = sec_struct_colors[idx]
+                start_idx = idx
+
+        # Plot the last segment
+        if current_color in colors.values():
+            plt.axvspan(start_idx, len(residue_ids), color=current_color, alpha=0.2)
+        legend_handles = [
+            mpatches.Patch(color='red', label='Helix (H)', alpha=0.2),  # Alpha uniforme
+            mpatches.Patch(color='blue', label='Beta sheet (E)', alpha=0.2)  # Alpha uniforme
+        ]
+        plt.legend(handles=legend_handles, loc='upper right')
+        # Create custom legend handles only for 'H' and 'E'
+        handles = [mlines.Line2D([0], [0], color=color, lw=4, label=struct, alpha=0.2) for struct, color in colors.items()]
+    
+        plt.legend(handles=handles, title='Secondary Structure', loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=2)
+
+        plt.xlabel('Residue Index')
+        plt.ylabel(ylabel)
+        plt.grid(True)
+
+        if not os.path.exists(f'images/{self.name}/Time_indicators/'):
+            os.makedirs(f'images/{self.name}/Time_indicators/')
+        
+        # Save the figure
+        plt.savefig(f'images/{self.name}/Time_indicators/{title}.png')
--- STATIC/Dynamic.py
+++ STATIC/Dynamic.py
@@ -5,6 +5,13 @@ from Visualize import Visualize
 from matrix import GraphMatrixAnalyzer
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import matplotlib.lines as mlines
+from scipy.stats import pearsonr
+import matplotlib.lines as mlines
+import os
+import matplotlib.patches as mpatches
 def stochastic_process_1d(K, epsilon_0, omega, dt, T, k_b, gamma, MaxTime, N):
     t = np.arange(0, MaxTime, dt)
     r_history = np.zeros((len(t), N))
@@ -35,21 +42,22 @@ def stochastic_process_1d(K, epsilon_0, omega, dt, T, k_b, gamma, MaxTime, N):
 
 
     return t, r_history,epsilon
-stringa="2m10"
-pdb_processor = PDBProcessor(pdb_id="2m10")
+stringa="3LNX"
+pdb_processor = PDBProcessor(pdb_id="3LNX")#2m07
 pdb_processor.download_pdb()
 pdb_processor.load_structure()
-
-# Extract data
 df1 = pdb_processor.secondary_structure()
 df = pdb_processor.extract_atom_data()
-print(df)
 df = df[df['Model ID'] == 0]
 df = df[df['Atom Name'] == 'CA']
-
+df = df[df['Chain ID'] == 'A']
 df = df.reset_index(drop=True)
 concatenated_df = pd.concat([df1['Secondary Structure'], df], axis=1)
 df = concatenated_df.dropna().reset_index(drop=True)
+df = df.loc[:,~df.T.duplicated()]
+df = concatenated_df.dropna().reset_index(drop=True)
+df = df.T.drop_duplicates().T
+visualizer = Visualize(df)
 # Initialize Visualize and analyze graph
 visualizer = Visualize(df)
 raggio=visualizer.calculate_and_print_average_distance()
@@ -107,24 +115,111 @@ plt.xlabel('Time Lag')
 plt.ylabel('Correlation')
 plt.title('Correlation between Residue 21 and Residue 76')
 plt.tight_layout()
-
 if not os.path.exists(f'images/{stringa}/dynamic/'):
     os.makedirs(f'images/{stringa}/dynamic/')
-
-# Salva la figura
 plt.savefig(f'images/{stringa}/dynamic/Correlation.png')
-# Plot the time average of x(t)^2 for each residue
-plt.figure(figsize=(12, 6))
-plt.plot(range(1, N+1), time_avg_x_squared, 'b-')
-plt.xlabel('Residue Number')
-plt.ylabel('Time Average of x(t)^2')
-plt.title('Time Average of x(t)^2 for Each Residue')
-plt.tight_layout()
-if not os.path.exists(f'images/{stringa}/dynamic/'):
-    os.makedirs(f'images/{stringa}/dynamic/')
 
-# Save the figure
-plt.savefig(f'images/{stringa}/dynamic/Stima beta factors.png')
+
+def compare_b_factors_with_sec_structure(actual_b_factors, predicted_b_factors, sec_struct_data, name):
+    # Scala i fattori B predetti per farli corrispondere all'intervallo dei fattori B reali
+    scale_factor = np.mean(actual_b_factors) / np.mean(predicted_b_factors)
+    predicted_b_factors_scaled = predicted_b_factors * scale_factor
+
+    # Calcola il coefficiente di correlazione
+    correlation, _ = pearsonr(actual_b_factors, predicted_b_factors_scaled)
+
+    # Calcola la deviazione quadratica media (RMSD)
+    rmsd = np.sqrt(np.mean((actual_b_factors - predicted_b_factors_scaled)**2))
+
+    # Crea il plot
+    fig, ax = plt.subplots(figsize=(12, 6))
+
+    # Plot dei fattori B reali
+    ax.set_xlabel('Indice del Residuo')
+    ax.set_ylabel('Fattori B')
+    ax.plot(actual_b_factors, label='Beta Factors', color='blue')
+
+    # Plot dei fattori B predetti
+    ax.plot(predicted_b_factors_scaled, label='Predicted Beta Factors', color='red')
+
+    # Aggiungi il titolo e la legenda
+    #plt.title('Confronto tra Fattori B Reali e Predetti')
+    ax.legend(loc="upper right")
+
+    # Aggiungi una griglia
+    ax.grid(True, alpha=0.3)
+
+    # Plot della struttura secondaria
+    sec_struct_info = sec_struct_data['Secondary Structure']
+    residue_ids = sec_struct_data['Residue ID'].astype(int)
+
+
+    colors = {'H': 'red', 'E': 'blue'}  # Escludiamo 'C' dalla mappatura dei colori
+
+    # Mappa i colori o None se la struttura è 'C'
+    sec_struct_colors = [colors.get(sec_struct_info.get(rid), None) for rid in residue_ids]
+
+    # Aggiungi le bande colorate per la struttura secondaria, escludendo 'C'
+    current_color = None
+    start_idx = 0
+    a=0.2
+
+    for idx, resid in enumerate(residue_ids):
+        if sec_struct_colors[idx] != current_color:
+            # Se non siamo nel primo residuo e l'attuale colore non è None, disegna una banda
+            if idx > 0 and current_color is not None:
+                ax.axvspan(start_idx, idx, color=current_color, alpha=a)  # Alpha uniforme
+            # Cambia il colore attuale e aggiorna l'indice di partenza
+            current_color = sec_struct_colors[idx]
+            start_idx = idx
+
+    # Aggiungi l'ultimo segmento se non è 'C' (None)
+    if current_color is not None:
+        ax.axvspan(start_idx, len(residue_ids), color=current_color, alpha=a)  # Alpha uniforme
+
+    # Creazione delle legende con lo stesso alpha per consistenza
+    legend_handles = [
+        mpatches.Patch(color='red', label='Helix (H)', alpha=a),  # Alpha uniforme
+        mpatches.Patch(color='blue', label='Beta sheet (E)', alpha=a)  # Alpha uniforme
+    ]
+
+    # Aggiungi la legenda con i nuovi handle
+    ax.legend(handles=legend_handles, loc='upper right')
+
+    # Crea la legenda personalizzata per la struttura secondaria
+    handles = [mlines.Line2D([0], [0], color=color, lw=4, label=struct, alpha=a) for struct, color in colors.items()]
+    ax.legend(handles=handles, title='Secondary Structure', loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=3)
+
+    
+    plt.tight_layout()
+
+    if not os.path.exists(f'images/{name}/dynamic/'):
+        os.makedirs(f'images/{name}/dynamic/')
+
+    # Salva la figura
+    plt.savefig(f'images/{name}/dynamic/Confronto_Beta_con_Struttura_Secondaria.png')
+
+    print(f"Coefficiente di correlazione tra fattori B reali e predetti: {correlation:.4f}")
+    print(f"Deviazione Quadratica Media (RMSD): {rmsd:.4f}")
+
+    return correlation, rmsd
+
+
+
+correlation,rmsd=compare_b_factors_with_sec_structure(df['B-Factor'].values, time_avg_x_squared, df, stringa)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Select residues to plot, including 20 and 75
 selected_residues = [20, 75]  # You can add more if needed
